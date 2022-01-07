8279f2a2c3f3d90ab2c1534e660d794789c3824b
Alexander Burachkov <zicio@yandex.ru>
2022-01-07 14:13:18 +0400

    Updated

--- index.js
+++ index.js
@@ -1,66 +1,75 @@
 const http = require('http');
 const Koa = require('koa');
+const koaBody = require('koa-body');
+const cors = require('koa-cors');
+
 const app = new Koa();
-// const cors = require('koa2-cors');
+app.use(cors());
 
-const tickets = [1, 2];
-console.log(tickets);
+// backend
+const backend = require('./server_backend');
 
-// app.use(async(ctx, next) => {
-//   const origin = ctx.request.get('Origin');
-//   const { method } = ctx.request.querystring;
-//   if (!origin) {
-//     return await next();
-//   }
-//   const headers = { 'Access-Control-Allow-Origin': '*' };
-//   if (ctx.request.method !== 'OPTIONS') {
-//     ctx.response.set({ ...headers });
-//     try {
-//       return await next();
-//     } catch (e) {
-//       e.headers = { ...e.headers, ...headers };
-//       throw e;
-//     }
-//   }
-//   if (ctx.request.get('Access-Control-Request-Method')) {
-//     ctx.response.set({
-//       ...headers,
-//       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH'
-//     });
-//     if (ctx.request.get('Access-Control-Request-Headers')) {
-//       ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
-//     }
+app.use(koaBody({
+  urlencoded: true
+}));
 
-//     switch (method) {
-//       case 'allTickets':
-//         ctx.response.body = tickets;
-//         return;
-//         // TODO: обработка остальных методов
-//       default:
-//         ctx.response.status = 404;
-//     }
-
-//     ctx.response.status = 204; // No content
-//   }
-// });
-
-// app.use(cors());
+/* data */
+const tickets = [
+  { id: 1, name: 'AAA' },
+  { id: 2, name: 'BBB' },
+  { id: 3, name: 'CCC' },
+  { id: 4, name: 'DDD' },
+  { id: 5, name: 'EEE' }
+];
 
 app.use(async ctx => {
-  const { method } = ctx.request.querystring;
-
-  ctx.response.set({
-    'Access-Control-Allow-Origin': '*'
-  });
-
-  switch (method) {
+  /* query information */
+  const method = ctx.request.method;
+  const route = ctx.request.query.method;
+  const qry = ctx.request.query;
+  // console.log('query=', qry);
+  /* handlers */
+  switch (route) {
+    // ---------------------
     case 'allTickets':
-      ctx.response.body = tickets;
-      return;
+      if (method === 'GET') {
+        ctx.response.body = tickets;
+        ctx.response.set({ 'Access-Control-Allow-Origin': '*' });
+        return;
+      }
+    // ---------------------
+    // case 'ticketById':
+    //   if (method === 'GET') {
+    //     const paramID = qry.id;
+    //     // console.log('id=', paramID);
+    //     const filteredTickets = tickets.filter(item => {
+    //       if (item.id === Number(paramID)) {
+    //         return true;
+    //       }
+    //     });
+    //     // console.log('filteredTickets=', filteredTickets);
+    //     ctx.response.body = filteredTickets;
+    //     ctx.response.set({ 'Access-Control-Allow-Origin': '*' });
+    //     return;
+    //   }
+    // // ---------------------
+    // case 'createTicket':
+    //   if (method === 'POST') {
+    //     const body = ctx.request.body;
+    //     console.log('post body=', body);
+    //     const newID = backend.getNewID(tickets);
+    //     const newTicket = { id: newID, name: body.name };
+    //     console.log('newTicket=', newTicket);
+    //     tickets.push(newTicket);
+    //   }
+    //   ctx.response.body = 'OK';
+    //   ctx.response.set({ 'Access-Control-Allow-Origin': '*' });
+    //   return;
+    // ---------------------
     default:
       ctx.response.status = 404;
+      ctx.response.set({ 'Access-Control-Allow-Origin': '*' });
   }
 });
 
-const port = process.env.PORT || 7070;
-const server = http.createServer(app.callback()).listen(port);
+app.listen(7070);
