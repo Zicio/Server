55651314a4b7a18aca9b6b4637d3b7f61f02f377
Alexander Burachkov <zicio@yandex.ru>
2022-01-09 22:27:22 +0400

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(koaBody({
 
 /* data */
 const tickets = [
-  { id: 1, name: 'AAA', status: true, created: '10.03.19 08:40'},
+  { id: 1, name: 'AAA', status: true, created: '10.03.19 08:40' },
   { id: 2, name: 'BBB', status: true, created: '10.03.19 08:57' },
   { id: 3, name: 'CCC', status: false, created: '10.03.19 08:20' },
   { id: 4, name: 'DDD', status: true, created: '10.03.19 08:37' },
-  { id: 5, name: 'EEE', status: false, created: '10.03.19 08:58'}
+  { id: 5, name: 'EEE', status: false, created: '10.03.19 08:58' }
 ];
 
 app.use(async ctx => {
@@ -43,7 +43,7 @@ app.use(async ctx => {
         // console.log('id=', paramID);
         const filteredTickets = tickets.filter(item => {
           if (item.id === Number(paramID)) {
-            return true,;
+            return true;
           }
         });
         // console.log('filteredTickets=', filteredTickets);
