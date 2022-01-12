bf6bfa2f708ef8049e15eb93b6132d4d2ee0ec74
Alexander Burachkov <zicio@yandex.ru>
2022-01-12 22:47:51 +0400

    Updated

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const Koa = require('koa');
 const koaBody = require('koa-body');
 const cors = require('@koa/cors');
+const uuid = require('uuid');
 
 const app = new Koa();
 app.use(cors());
 
 app.use(koaBody({
-  urlencoded: true
+  urlencoded: true,
+  multipart: true
 }));
 
 /* data */
@@ -40,31 +42,30 @@ app.use(async ctx => {
     case 'ticketById':
       if (method === 'GET') {
         const paramID = qry.id;
-        // console.log('id=', paramID);
         const filteredTickets = tickets.filter(item => {
           if (item.id === Number(paramID)) {
             return true;
           }
         });
-        // console.log('filteredTickets=', filteredTickets);
         ctx.response.body = filteredTickets;
         ctx.response.set({ 'Access-Control-Allow-Origin': '*' });
         return;
       }
     // ---------------------
-    // case 'createTicket':
-    //   if (method === 'POST') {
-    //     const body = ctx.request.body;
-    //     console.log('post body=', body);
-    //     const newID = backend.getNewID(tickets);
-    //     const newTicket = { id: newID, name: body.name };
-    //     console.log('newTicket=', newTicket);
-    //     tickets.push(newTicket);
-    //   }
-    //   ctx.response.body = 'OK';
-    //   ctx.response.set({ 'Access-Control-Allow-Origin': '*' });
-    //   return;
-    // // ---------------------
+    case 'createTicket':
+      if (method === 'POST') {
+        const body = ctx.request.body;
+        console.log('post body=', body);
+        const { v4: uuidv4 } = require('uuid');
+        const newID = uuidv4();
+        const newTicket = { id: newID, name: body.name };
+        console.log('newTicket=', newTicket);
+        tickets.push(newTicket);
+      }
+      ctx.response.body = 'OK';
+      ctx.response.set({ 'Access-Control-Allow-Origin': '*' });
+      return;
+    // ---------------------
     default:
       ctx.response.status = 404;
       ctx.response.set({ 'Access-Control-Allow-Origin': '*' });
